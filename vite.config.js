61d8fde20e6aadee1401184fe21f7c2cd75f96b3
dfsrf <2655524539@qq.com>
2025-12-27 11:51:31 +0800

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,14 @@ import { resolve } from 'path'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
-export default defineConfig({// 修改基础路径
+export default defineConfig({// 修改基础路
+  base: '/EcoTrack/',
   server: {
     port: 8081, // 修改端口
     host: 'localhost',
     open: true
   },
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' 
-    ? '/EcoTrack/'  // 替换为你的仓库名
-    : '/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
